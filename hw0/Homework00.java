44dff589e895e3280508b5233788962aece6b0c0
Simarjeet Kaur <simarjeet@berkeley.edu>
2019-09-06 02:43:17 -0700

    hw0 first

---
+++ hw0/Homework00.java
@@ -0,0 +1,20 @@
+/** find max of array of integers
+	*/
+
+public static int max(int[] a) {
+	int i = 0;
+	int greatest = a[0]
+	for (i === 0; a.length > i, i += 1) {
+		if (a[i] > greatest) {
+			greatest = a[i];
+		}
+		return greatest
+		}
+	}
+
+/** 3sum - asks if there are three 
+integers in a[] whose sum is zero */
+
+public static boolean threeSum(int[] a) {
+	for 
+}
