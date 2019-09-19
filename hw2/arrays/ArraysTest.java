3f64f0855bca5de9c593d2806b53ea0b006d013e
Simarjeet Kaur <simarjeet@berkeley.edu>
2019-09-18 23:51:08 -0700

    edits

--- hw2/arrays/ArraysTest.java
+++ hw2/arrays/ArraysTest.java
@@ -11,6 +11,14 @@ public class ArraysTest {
     /**
      */
     @Test
+    public void catenateTest() {
+        int[] example = {1, 2};
+        int[] example2 = {3, 4};
+        int[] expected = {1, 2, 3, 4};
+        int[] result = Arrays.catenate(example, example2);
+        boolean isTrue = Utils.equals(result, expected);
+        assertEquals(isTrue, true);
+    }
 //    ArrayList example = ArrayList.list(1, 3, 7, 5, 4, 6, 9, 10, 10, 11);
 //    IntList example2 = IntList.list(1);
 //    assertEquals(Lists.naturalRuns(example), new int[][] {{1, 3, 7}, {5}, {4, 6, 9, 10}, {10, 11}});
