157eb8764750343c37947e3afd8d3178bf2a5eea
Simarjeet Kaur <simarjeet@berkeley.edu>
2019-10-15 12:01:17 -0700

    edited Machine

--- proj1/enigma/Machine.java
+++ proj1/enigma/Machine.java
@@ -1,5 +1,7 @@
 package enigma;
 
+import javax.management.AttributeList;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collection;
 
@@ -13,26 +15,38 @@ class Machine {
     /** A new Enigma machine with alphabet ALPHA, 1 < NUMROTORS rotor slots,
      *  and 0 <= PAWLS < NUMROTORS pawls.  ALLROTORS contains all the
      *  available rotors. */
+
+    int _numRotors;
+    int _pawls;
+    Permutation _plugboard;
+    Collection<Rotor> _allRotors;
+
     Machine(Alphabet alpha, int numRotors, int pawls,
             Collection<Rotor> allRotors) {
         _alphabet = alpha;
+        _numRotors = numRotors;
+        _pawls = pawls;
+        Collection<Rotor> _allRotors = allRotors;
         // FIXME
     }
 
     /** Return the number of rotor slots I have. */
     int numRotors() {
-        return 0; // FIXME
+        return _numRotors; // FIXME
     }
 
     /** Return the number pawls (and thus rotating rotors) I have. */
     int numPawls() {
-        return 0; // FIXME
+        return _pawls; // FIXME
     }
 
     /** Set my rotor slots to the rotors named ROTORS from my set of
      *  available rotors (ROTORS[0] names the reflector).
      *  Initially, all rotors are set at their 0 setting. */
     void insertRotors(String[] rotors) {
+        //what is a rotor slot
+        //this gives a stringof the correct order of rotors, and you need to reorder the collection properly
+        Collections.sort(_allRotors);
         // FIXME
     }
 
@@ -40,19 +54,30 @@ class Machine {
      *  numRotors()-1 characters in my alphabet. The first letter refers
      *  to the leftmost rotor setting (not counting the reflector).  */
     void setRotors(String setting) {
+        //does this mean change the setting of each rotor to what is given?
+        //use set in rotor class
+        int i = 0;
+        for (Rotor rotor : _allRotors) {
+            rotor.set(setting.charAt(i));
+            i++;
+        }
         // FIXME
     }
 
     /** Set the plugboard to PLUGBOARD. */
     void setPlugboard(Permutation plugboard) {
-        // FIXME
+        _plugboard = plugboard;
     }
 
     /** Returns the result of converting the input character C (as an
      *  index in the range 0..alphabet size - 1), after first advancing
-
      *  the machine. */
     int convert(int c) {
+        //is this using permute with a new permutation?
+        //write easy test cases first
+        //jumping from rotor to rotor using convert
+        //next rotor next cycle that it's in
+        //advancing machine = next rotor
         return 0; // FIXME
     }
 
